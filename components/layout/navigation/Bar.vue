4b2248a0f0e677a84e610572386606e21518c83a
N-eeraj <neeraj7rajeena@gmail.com>
2023-08-10 13:04:14 +0530

    Added Financia link in the nav drawer

--- components/layout/navigation/Bar.vue
+++ components/layout/navigation/Bar.vue
@@ -2,18 +2,18 @@
   <div class="flex justify-between w-full">
     <div class="flex items-center gap-x-3">
       <button class="lg:hidden" @click="toggleNavDrawer">
-        <Icon :name="navDrawerVisibility ? 'ion:close' : 'eva:menu-outline'" class="max-md:relative text-theme-light text-6xl" />
+        <Icon :name="navDrawerVisibility ? 'ion:close' : 'eva:menu-outline'" class="text-theme-light text-6xl max-md:relative" />
       </button>
-      <span class="text-theme-grey-light text-3xl max-sm:hidden">
+      <span class="text-theme-grey-light text-3xl max-md:hidden">
         Dashboard
       </span>
     </div>
 
     <div class="flex items-center gap-x-5">
-      <Icon name="mdi-notifications" size="32" class="w-14 h-14 p-3 aspect-square text-theme-grey-light max-sm:!hidden" />
+      <Icon name="mdi-notifications" size="32" class="w-14 h-14 p-3 aspect-square text-theme-grey-light max-md:!hidden" />
 
       <div class="flex items-center gap-x-3">
-        <span class="text-theme-light text-xl max-sm:hidden">
+        <span class="text-theme-light text-xl max-md:hidden">
           John Doe
         </span>
         <img src="/users/john-doe.png" class="w-14 rounded-full" />
