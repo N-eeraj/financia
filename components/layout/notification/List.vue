1d06db30aeaab24ddddabe93537baa204ff64527
N-eeraj <neeraj7rajeena@gmail.com>
2023-08-21 13:01:56 +0530

    Added read all notifications functionality & minor style update

--- components/layout/notification/List.vue
+++ components/layout/notification/List.vue
@@ -7,13 +7,14 @@
           <LayoutNotificationItem v-for="({ id, title, body, time }) of allNotifications" :id="id" :title="title" :body="body" :time="time" class="border-t border-t-theme-light first-of-type:border-t-0" :key="id" />
         </div>
 
-        <button class="w-full p-2 bg-theme-grey-dark/50 text-theme-light">
+        <button class="w-full p-2 bg-theme-grey-dark/50 text-theme-light duration-200 hover:bg-theme-grey-light/50" @click="readAllNotification">
           Read All Notifications
         </button>
       </template>
 
-    <div v-if="notificationListVisibility" class="absolute top-16 right-0 flex-column items-start gap-y-3 w-80 max-h-96 bg-gradient-to-bl glass-bg rounded-md border-[0.25px] border-theme-grey-light overflow-auto" :class="[classes]">
-      <LayoutNotificationItem v-for="({ id, title, body, time }) of notificationsData" :id="id" :title="title" :body="body" :time="time" class="border-t border-t-theme-light first-of-type:border-t-0" :key="id" />
+      <div v-else class="place-center w-60 h-48">
+        No Notifications
+      </div>
     </div>
   </Transition>
 </template>
@@ -31,4 +32,5 @@ defineProps({
 
 const mainStore = useMainStore()
 const { allNotifications } = storeToRefs(mainStore)
+const { readAllNotification } = mainStore
 </script>
