b3179177bf65b33d72d2dc074dbb17207344f381
N-eeraj <neeraj7rajeena@gmail.com>
2023-08-22 15:34:27 +0530

    Updates to home navigation checks

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
     viewportWidth: 1440,
     viewportHeight: 960,
     setupNodeEvents(on, config) {
