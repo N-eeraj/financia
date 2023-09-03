a0498406e3bd9f792896e5c40de3a0d76eaec5b8
N-eeraj <neeraj7rajeena@gmail.com>
2023-09-03 12:15:23 +0530

    Re-validate confirm password on password change

--- pages/sign-up.vue
+++ pages/sign-up.vue
@@ -2,7 +2,7 @@
   <EntryForm :image="image" :title="title" :subtitle="subtitle" :segue="segue" :link="link" @submit="handleSignUp">
     <BaseInput v-model="form.name" :ref="el => formInput.name = el" :validator="nameValidation" :placeholder="placeholder.name" variant="clear" dark class="w-full" data-cy="name-input" />
     <BaseInput v-model="form.email" :ref="el => formInput.email = el" type="email" :validator="emailValidation" :placeholder="placeholder.email" variant="clear" dark class="w-full" data-cy="email-input" />
-    <BaseInput v-model="form.password" :ref="el => formInput.password = el" type="password" :validator="passwordValidation" :placeholder="placeholder.password" variant="clear" dark class="w-full" data-cy="password-input" />
+    <BaseInput v-model="form.password" :ref="el => formInput.password = el" type="password" :validator="passwordValidation" :placeholder="placeholder.password" variant="clear" dark class="w-full" data-cy="password-input" @change="handlePasswordChange" /> />
     <BaseInput v-model="form.confirmPassword" :ref="el => formInput.confirmPassword = el" type="password" :validator="handlePasswordConfirmation" :placeholder="placeholder.confirmPassword" variant="clear" dark class="w-full" data-cy="confirm-password-input" />
     <BaseButton variant="gradient" color="blue" dark data-cy="sign-up-button">
       {{ button }}
@@ -20,12 +20,8 @@ definePageMeta({
   user: false,
 })
 
-type TemplateRef = Element | ComponentPublicInstance | null
 interface LoginFormInput {
-  name: TemplateRef
-  email: TemplateRef
-  password: TemplateRef
-  confirmPassword: TemplateRef
+  [key: string]: Element | ComponentPublicInstance | null
 }
 // form field references
 const formInput = reactive<LoginFormInput>({
@@ -44,6 +40,8 @@ const form = reactive({
 })
 
 const handlePasswordConfirmation = (value: any) => confirmValidation(form.password, value, 'passwords')
+
+const handlePasswordChange = () => form.confirmPassword && formInput.confirmPassword?.validate()
 
 const handleSignUp = () => {
   if (!validateForm(formInput)) return
