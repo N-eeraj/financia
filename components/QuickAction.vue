410e0a96a4e521d7aa7bf1502713786c50f6c8e2
N-eeraj <neeraj7rajeena@gmail.com>
2023-09-15 21:56:41 +0530

    Added quick action component

---
+++ components/QuickAction.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="flex justify-center items-center gap-x-2 px-3 py-2 bg-theme-dark hover:bg-base-blue/25 rounded-md duration-300 group">
+    <Icon v-if="icon" :name="icon" class="p-2 bg-base-blue/10 group-hover:bg-base-blue text-base-blue group-hover:text-theme-light text-4xl rounded-full" />
+    <span class="text-theme-light text-sm">
+      {{ text }}
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: false,
+  },
+})
+</script>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <BankAccount :bank-id="1" :account-number="1234123412341234" ifsc="ICI1234" :account-type="1" :balance="123456" />
+    <QuickAction text="Action Name" icon="carbon:receipt" />
   </div>
 </template>
 
