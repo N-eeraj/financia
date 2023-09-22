a55458c89611e62b58996f493fc86874930cf42e
N-eeraj <neeraj7rajeena@gmail.com>
2023-09-22 21:28:17 +0530

    My profile actions container

--- components/Help.vue
+++ components/Help.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed top-1/2 left-1/2 flex-column gap-y-3 p-5 bg-gradient-to-bl glass-bg text-theme-light border border-theme-grey-light rounded-md -translate-x-1/2 -translate-y-1/2" @click.stop>
-    <strong class="font-medium">
+    <strong class="font-medium text-2xl">
       Help
     </strong>
     <p class="w-72 md:w-96 lg:w-[480px]">
--- pages/dashboard/profile.vue
+++ pages/dashboard/profile.vue
@@ -1,10 +1,33 @@
 <template>
   <div>
-    Profile
+    <ProfileActionContainer :active-hash="activeTab" />
   </div>
 </template>
 
 <script setup lang="ts">
+const route = useRoute()
+
+const activeTab = ref('')
+
+const showProfile = () => activeTab.value = ''
+const showChangePassword = () => activeTab.value = '#change-password'
+const showDeleteAccount = () => console.log('showDeleteAccount')
+
+watch(
+  () => route.hash,
+  (to, from) => {
+    switch (route.hash) {
+      case '#change-password':
+        return showChangePassword()
+      case '#delete-account':
+        return showDeleteAccount()
+      default:
+        return showProfile()
+    }
+  },
+  { immediate: true }
+)
+
 definePageMeta({
   ...authDashboard,
   pageTitle: 'Profile',
