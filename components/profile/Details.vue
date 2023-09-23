432e5689a9d5208e51b96e74a99b35099c587a44
N-eeraj <neeraj7rajeena@gmail.com>
2023-09-23 14:48:21 +0530

    Added profile details form

--- components/profile/Details.vue
+++ components/profile/Details.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <img :src="profilePicture || `${baseURL}/users/john-doe.png`" class="w-14 rounded-full" />
+  <div class="flex-column items-center gap-y-12 w-full">
+    <div class="relative w-48 aspect-square">
+      <img :src="profilePicture || `${baseURL}/users/john-doe.png`" class="w-full h-full rounded-full" />
+      <Icon name="mdi-camera" size="48" class="absolute right-0 bottom-0 p-3 bg-base-green text-theme-light rounded-full" />
+    </div>
+
+    <form class="flex-column items-end gap-y-8 w-full max-w-full" @submit="handleSubmit">
+      <BaseInput v-model="form.name" :ref="el => formInput.name = el" :validator="requiredValidation" placeholder="Full Name" variant="clear" dark class="w-full" />
+      <BaseInput v-model="form.email" :ref="el => formInput.email = el" :validator="requiredValidation" placeholder="Email" variant="clear" dark class="w-full" />
+      <BaseInput v-model="form.phone" :ref="el => formInput.phone = el" placeholder="Phone Number" variant="clear" dark class="w-full" />
+
+      <BaseButton variant="gradient" color="blue" dark>
+        Save
+      </BaseButton>
+    </form>
   </div>
 </template>
 
@@ -10,6 +23,31 @@ import { storeToRefs } from 'pinia'
 const userStore = useUserStore()
 const { profilePicture } = storeToRefs(userStore)
 
+type TemplateRef = Element | ComponentPublicInstance | null
+interface ProfileFormInput {
+  name: TemplateRef
+  email: TemplateRef
+  phone: TemplateRef
+}
+// form field references
+const formInput = reactive<ProfileFormInput>({
+  name: null,
+  email: null,
+  phone: null,
+})
+
 const runtime = useRuntimeConfig()
 const baseURL = runtime.public.baseURL
+
+const form = reactive({
+  name: '',
+  email: '',
+  phone: '',
+})
+
+const handleSubmit = (event: Event) => {
+  event.preventDefault()
+  if (!validateForm(formInput)) return
+  console.log('form Submit')
+}
 </script>
