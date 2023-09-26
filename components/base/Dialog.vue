046c97ad8621aeffdc86d04593deb804c5cdd57b
N-eeraj <neeraj7rajeena@gmail.com>
2023-09-26 13:43:37 +0530

    Updations in dialog styles

--- components/base/Dialog.vue
+++ components/base/Dialog.vue
@@ -1,31 +1,33 @@
 <template>
-  <!-- overlay -->
-  <div v-if="overlay" class="fixed top-0 left-0 w-screen h-screen bg-gradient-to-bl glass-bg z-50" />
+  <div class="full-screen" :class="show ? 'z-50' : ' -z-50 duration-1000'">
+    <!-- overlay -->
+    <div v-if="overlay && show" class="full-screen bg-gradient-to-bl glass-bg" />
 
-  <Transition name="slide-up">
-    <!-- dialog -->
-    <div v-if="show" class="screen-center flex-column gap-y-5 w-80 md:w-[480px] p-10 glass-bg border border-theme-light rounded-md z-50" @click.stop>
+    <Transition name="fade">
+      <!-- dialog -->
+      <div v-if="show" class="screen-center flex-column gap-y-5 w-80 md:w-[480px] p-10 glass-bg border border-theme-light rounded-md" @click.stop>
 
-      <!-- title -->
-      <strong class="text-theme-light text-3xl font-semibold">
-        <slot name="title">
-          <template v-if="title">
-            {{ title }}
-          </template>
-        </slot>
-      </strong>
+        <!-- title -->
+        <strong class="text-theme-light text-3xl font-semibold">
+          <slot name="title">
+            <template v-if="title">
+              {{ title }}
+            </template>
+          </slot>
+        </strong>
 
-      <!-- body -->
-      <div class="text-theme-light">
-        <slot />
+        <!-- body -->
+        <div class="text-theme-light">
+          <slot />
+        </div>
+
+        <!-- actions container -->
+        <div class="flex justify-end gap-x-4">
+          <slot name="actions" />
+        </div>
       </div>
-
-      <!-- actions container -->
-      <div class="flex justify-end gap-x-4">
-        <slot name="actions" />
-      </div>
-    </div>
-  </Transition>
+    </Transition>
+  </div>
 </template>
 
 <script setup lang="ts">
