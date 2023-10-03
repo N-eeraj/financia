2381d0ccf794c6079bcc38441b932bba77d49442
N-eeraj <neeraj7rajeena@gmail.com>
2023-10-03 13:09:09 +0530

    Added more validations to update profile form

--- cypress/e2e/commands/profile/update.cy.js
+++ cypress/e2e/commands/profile/update.cy.js
@@ -1,11 +1,21 @@
 import { dashboard } from '/cypress/fixtures/url.json'
 import { validCredentials } from '/cypress/fixtures/entry-form/login.json'
 import { loginUserWithIndex } from '/cypress/e2e/helpers/login.cy.js'
+
 import {
   checkInputValue,
   checkEmptyValidation,
+  checkInvalidInput,
+  checkValidInput,
   checkRequiredInputCount,
+  checkMinMaxInputLength,
 } from '/cypress/e2e/helpers/inputValidations.cy.js'
+
+
+import {
+  invalidEmails,
+  validEmails,
+} from '/cypress/fixtures/entry-form/emails.json'
 
 // helper function to visit sign-up page and alias form input fields
 const updateFormInit = (userIndex = 0) => {
@@ -16,12 +26,12 @@ const updateFormInit = (userIndex = 0) => {
   cy.get('[data-cy="phone"]').find('[data-cy="input"]').as('phoneInputField')
 }
 
-
 const clearForm = () => {
   cy.get('@nameInputField').clear()
   cy.get('@emailInputField').clear()
   cy.get('@phoneInputField').clear()
 }
+
 
 const checkCurrentUserDetails = () => {
   const userIndex = Math.floor(Math.random() * validCredentials.length)
@@ -57,8 +67,52 @@ const checkRequiredFields = () => {
   })
 }
 
-describe('User Update', () => {
+const checkNameInputLength = () => {
+  updateFormInit()
+  clearForm()
+  checkMinMaxInputLength({
+    selector: 'name',
+    min: {
+      length: 2,
+      errorMsg: 'Name must be atleast 2 characters long',
+    },
+  })
+}
+
+const checkInvalidEmailInputs = () => {
+  updateFormInit()
+  invalidEmails.forEach(email => {
+    cy.reload()
+    clearForm()
+    checkInvalidInput({
+      selector: 'email',
+      input: email,
+      errorMsg: 'Please enter a valid email',
+    })
+  })
+}
+
+const checkValidEmailInputs = () => {
+  updateFormInit()
+  validEmails.forEach(email => {
+    cy.reload()
+    clearForm()
+    checkValidInput({
+      selector: 'email',
+      input: email,
+    })
+  })
+}
+
+describe('User Details', () => {
   it('Checks current user details', checkCurrentUserDetails)
+})
+
+describe('User Update Validations', () => {
   it('Checks empty input validations', checkEmptyValidations)
   it('Checks required fields', checkRequiredFields)
+  it('Checks length requirement for name input', checkNameInputLength)
+  it('Checks invalid email errors', checkInvalidEmailInputs)
+  it('Checks valid emails', checkValidEmailInputs)
 })
+
